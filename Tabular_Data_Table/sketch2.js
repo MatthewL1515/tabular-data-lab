fd76126a21f846973081a1a3d2006d4d17b1caf3
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-01-20 14:20:08 -0500

    Add files via upload

---
+++ Tabular_Data_Table/sketch2.js
@@ -0,0 +1,9 @@
+// https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/drug-use-by-age/drug-use-by-age.csv
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
