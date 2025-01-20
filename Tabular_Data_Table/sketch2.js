f055bfaddf233399fd11e76dbfea07cc5b11358a
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-01-20 18:03:09 -0500

    Added in title and basic functions

--- Tabular_Data_Table/sketch2.js
+++ Tabular_Data_Table/sketch2.js
@@ -1,20 +1,18 @@
-// Raw Data Site: https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv
+// Monthy Data
 
-// Data Site: https://github.com/fivethirtyeight/data/blob/master/us-weather-history/KNYC.csv
+const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/master/us-weather-history/KNYC.csv"
 
-const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv"
+let weatherTable
 
 function preload() {
-  weatherTable = loadTable(weatherURL,'csv','header')
+  weatherTable = loadTable(weatherURL, 'csv', 'header')
 }
 
-function canvas() {
-  createCanvas(400,400)
+function setup() {
+  createCanvas(500, 500)
   noLoop()
 }
 
 function draw() {
   background(220)
-  stroke(0)
-  fill(0)
 }
