62c5d4c7fcb5ba1cf90b13d88ee455c016105677
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-01-20 17:04:43 -0500

    Put in dotted data into canvas
    For some reason, when I was using the old file to draw, nothing was showing up. It worked when I copied and pasted the code into a new document though, so I'm a bit confused about what caused that.

--- Tabular_Data_Table/sketch1.js
+++ Tabular_Data_Table/sketch1.js
@@ -1,19 +1,14 @@
-// Raw Data Site: https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv
+const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/master/us-weather-history/KNYC.csv"
 
-// Data Site: https://github.com/fivethirtyeight/data/blob/master/us-weather-history/KNYC.csv
-
-
-
-// Variables
-const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/us-weather-history/KNYC.csv"
+let weatherTable
 
   // Array variables
-  let tempsMax = []
-  let tempsMin = []
+let tempsMax = []
+let tempsMin = []
   
   // Initialized Variables
-  let minTemp = -Infinity
-  let maxTemp = Infinity
+let minTemp = Infinity
+let maxTemp = -Infinity
 
 function preload() {
   weatherTable = loadTable(weatherURL,'csv','header')
@@ -34,16 +29,36 @@ function setup() {
     tempsMin.push(minT)
     
     // Track the temperatures
-    if (maxT > maxTemp)
+    if (maxT > maxTemp) {
       maxTemp = maxT
-    if (minT < minTemp)
+    }
+    if (minT < minTemp) {
       minTemp = minT
+    }
   }
 }
 
 function draw() {
   background(220)
-  stroke(0)
-  fill(0)
+
+  // Spacing btween points
+  let dx = width / tempsMax.length
   
+  for (let i = 0; i < tempsMax.length; i++) {
+    let tMax = tempsMax[i]
+    let tMin = tempsMin[i]
+    let x = (i + 1) * dx
+    let yMax = map(tMax, minTemp, maxTemp, height - 50, 50)
+    let yMin = map(tMin, minTemp, maxTemp, height - 50, 50)
+    
+    // Draw points onto screen
+      // Max
+    stroke("red")
+    point(x, yMax)
+    
+      // Min
+    stroke("blue")
+    point(x, yMin)
+    
+  }
 }
