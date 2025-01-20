ecbd2746bb1c9e6293f219766b2b5d39e196196a
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-01-20 13:37:24 -0500

    Delete Tabular_Data - Drug Use by Age directory
    Reason: I didn't realize it had to be about the demo data

--- Tabular_Data - Drug Use by Age/sketch.js
+++
@@ -1,9 +0,0 @@
-// https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/drug-use-by-age/drug-use-by-age.csv
-
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-}
