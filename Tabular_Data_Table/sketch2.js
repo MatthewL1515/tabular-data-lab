8c8c780d614aaf3e41c8b159164686cfed593419
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-01-20 20:36:33 -0500

    Storing temperature data in an array

--- Tabular_Data_Table/sketch2.js
+++ Tabular_Data_Table/sketch2.js
@@ -4,6 +4,16 @@ const weatherURL = "https://raw.githubusercontent.com/fivethirtyeight/data/maste
 
 let weatherTable
 
+// Array Variables
+let tempsMax = []
+let tempsMin = []
+let months = []
+
+// Initialized Variables
+let minTemp = Infinity
+let maxTemp = -Infinity
+let selectedMonth = 1
+
 function preload() {
   weatherTable = loadTable(weatherURL, 'csv', 'header')
 }
@@ -11,6 +21,30 @@ function preload() {
 function setup() {
   createCanvas(500, 500)
   noLoop()
+  
+  // Extract Temperature data for each row
+  for (let i = 0; i < weatherTable.getRowCount(); i++) {
+    let dataStr = weatherTable.getString(i, "date")
+    let maxT = weatherTable.getNum(i, "actual_max_temp")
+    let minT = weatherTable.getNum(i, "actual_min_temp")
+    
+    let month = int(dateStr.split('-')[1])
+    // Note: I had to look up how to do this, because I was unsure how to identify and single out the date. The int() brings out the value as number (because dataStr brings it out as a string). Then the .split('-') tells the function to search for the numbers seperated by dashes '-'. The bracketed 1 [1] finds the second number in the string, which is the month I'm looking for.
+    
+    // Store in the seperate arrays
+    months.push(month)
+    maxTemps.push(maxT)
+    minTemps.push(minT)
+    
+    // Track min and max temperatures
+    if (maxT > maxTemp) {
+      maxTemp = maxT
+    }
+    
+    if (minT > minTemp) {
+      minTemp = minT
+    }
+  }
 }
 
 function draw() {
